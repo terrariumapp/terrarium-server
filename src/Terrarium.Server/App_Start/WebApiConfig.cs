617d385a793d820d2377664ab04e940f94a3f9f6
Bil Simser <emailme@bilsimser.com>
2014-06-15 09:05:54 -0600

    Stubbed out rest of API now cleaning up interfaces (getting rid of stupid DataSets and "out" parameters)

--- src/Terrarium.Server/App_Start/WebApiConfig.cs
+++ src/Terrarium.Server/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace Terrarium.Server
 {
@@ -16,6 +17,9 @@ namespace Terrarium.Server
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Default all responses to JSON coming from browsers
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
