ea5378524409453b34d6cd509f4c876b1a5c01f0
Bil Simser <emailme@bilsimser.com>
2014-06-14 22:59:44 -0600

    Initial setup of Web API server

---
+++ src/Terrarium.Server/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Terrarium.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ src/Terrarium.Server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Terrarium.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
