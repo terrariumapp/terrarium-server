0af4eaa75f6eb44f5f10243b632c9753cc0f852d
Bil Simser <emailme@bilsimser.com>
2014-06-19 06:52:28 -0600

    Home page content tweaks

--- src/Terrarium.Server/Global.asax.cs
+++ src/Terrarium.Server/Global.asax.cs
@@ -8,10 +8,20 @@ namespace Terrarium.Server
 {
     public class WebApiApplication : HttpApplication
     {
+        /// <summary>
+        /// Gets this party started.
+        /// </summary>
+        /// <remarks>
+        /// The database will be created (locally) automatically but
+        /// if you want to seed the database with some values just
+        /// add this line below:
+        /// <code>
+        ///     System.Data.Entity.Database.SetInitializer(new TerrariumDbSeedInitializer());
+        /// </code>
+        /// and modify the TerrariumDbSeedInitiailizer class with whatever values you want.
+        /// </remarks>
         protected void Application_Start()
         {
-            //TODO uncomment this to get a new database created
-            //System.Data.Entity.Database.SetInitializer(new TerrariumDbSeedInitializer());
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
             MvcHandler.DisableMvcResponseHeader = true;
